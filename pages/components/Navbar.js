f4dd5d39865c0b263832eab2a60e0e47e9ec0c66
Kaique <kaique.santos@duosystem.com.br>
2023-05-27 20:08:17 -0300

    Getting pokemons

---
+++ pages/components/Navbar.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import style from '../../styles/Navbar.module.css';
+
+export default function NavBar(){
+    return (
+        <nav className={style.navbar}>
+            <div className={style.logo}>
+                <Image src="/images/pokeball.png" width="30" height="30" alt="PokeNext" />
+                <h1>PokeNext</h1>
+            </div>
+            <ul className={style.link_items}>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about">Sobre</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
